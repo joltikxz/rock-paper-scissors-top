8bdc730db7992739edf6796b0db5a0fcb0673836
joltikxz <adrianpaulbarameda@gmail.com>
2025-05-03 00:03:43 +0800

    Creation of UI

--- rock_paper_scissors.js
+++ rock_paper_scissors.js
@@ -1,6 +1,40 @@
 let humanScore = 0;
 let computerScore = 0;
 
+let rock = document.querySelector('.rock-btn');
+let paper = document.querySelector('.paper-btn');
+let scissors = document.querySelector('.scissors-btn');
+let scoreBoard = document.querySelector('.scoreboard')
+
+let paraHumanScore = document.createElement('p');
+let paraComputerScore = document.createElement('p');
+
+scoreBoard.appendChild(paraHumanScore);
+scoreBoard.appendChild(paraComputerScore);
+        
+rock.addEventListener('click', () => {
+    scoreBoard.removeChild(paraHumanScore);
+    scoreBoard.removeChild(paraComputerScore);
+    playGame('rock');
+    scoreChecker();
+});
+
+paper.addEventListener('click', () => {
+    scoreBoard.removeChild(paraHumanScore);
+    scoreBoard.removeChild(paraComputerScore);
+    playGame('paper');
+    scoreChecker();
+});
+
+
+scissors.addEventListener('click', () => {
+    scoreBoard.removeChild(paraHumanScore);
+    scoreBoard.removeChild(paraComputerScore);
+    playGame('scissors');
+    scoreChecker();
+});
+
+
 //Function for the game logic
 function getComputerChoice() {
     let randInt = Math.floor(Math.random() * 3);
@@ -15,45 +49,64 @@ function getComputerChoice() {
 }
 
 //Function for retrieving the choice of the user
-function getHumanChoice(roundNum) {
-    let humanChoice = prompt(`Round ${roundNum}\nPlease enter your choice (rock, paper, scissors): `);;
+function getHumanChoice() {
+    //let humanChoice = prompt(`Round ${roundNum}\nPlease enter your choice (rock, paper, scissors): `);
 
-    return humanChoice.toLowerCase();
+    //return humanChoice.toLowerCase();
+    if (choice === 'rock') {
+        return 'rock';
+    } else if (choice === 'paper') { 
+        return 'paper';
+    } else if (choice === 'scissors') { 
+        return 'scissors';
+    }
 }
 
 //Function for playing a single round with a human choice, computer choice, and a round number as parameters
-function playRound(humanChoice, computerChoice, roundNum) {
+function playRound(humanChoice, computerChoice) {
     if ((humanChoice == "rock" && computerChoice == "scissors") ||
         (humanChoice == "paper" && computerChoice == "rock") ||
         (humanChoice == "scissors" && computerChoice == "paper")) {
             
-            alert(`Congrats! You won Round ${roundNum}`);
+            alert(`Congrats! You won this round`);
             humanScore += 1;
 
     } else if ((computerChoice == "rock" && humanChoice == "scissors") ||
                (computerChoice == "paper" && humanChoice == "rock") ||
                (computerChoice == "scissors" && humanChoice == "paper")) {
 
-            alert(`Oops! You lost Round ${roundNum}`);
+            alert(`Oops! You lost this round`); 
             computerScore += 1;
 
     } else {
-            alert(`Who would've thought? It was a draw for Round ${roundNum}!`);
+            alert(`Who would've thought? It was a draw for this round!`);
     } 
 }
 
 //Function for playing the game with 5 rounds and new humanChoice and computerChoice for each rounds
-function playGame() {
-    for (let i=1; i<=5; i++) {
-        let humanChoice = getHumanChoice(i);
-        let computerChoice = getComputerChoice();
+function playGame(humanChoice) {
+    let computerChoice = getComputerChoice();
 
-        playRound(humanChoice, computerChoice, i);
+    playRound(humanChoice, computerChoice);
+
+    paraHumanScore.textContent = `Your score: ${humanScore}`;
+    paraComputerScore.textContent = `Computer score: ${computerScore}`;
+
+    scoreBoard.appendChild(paraHumanScore);
+    scoreBoard.appendChild(paraComputerScore);
+}
+
+function scoreChecker() {
+    if (humanScore == 5) {
+        alert(`Congrats! You won the game\nYour score: ${humanScore}\nComputer score : ${computerScore}`);
+        location.reload();
+    } else if (computerScore == 5) {
+        alert(`You lost the game! Better luck next time\nYour score: ${humanScore}\nComputer score : ${computerScore}`);
+        location.reload();
     }
 }
 
-playGame();
-
+/*
 if (humanScore > computerScore) {
     alert(`Congrats! You won the game.\nAcross 5 rounds, here are the scores:\nYour score: ${humanScore}\nComputer score: ${computerScore}`);
 } else if (humanScore < computerScore) {
@@ -61,3 +114,4 @@ if (humanScore > computerScore) {
 } else {
     alert(`What are the chances? It was a draw!\nAcross 5 rounds, here are the scores:\nYour score: ${humanScore}\nComputer score: ${computerScore}`);              
 }
+*/
